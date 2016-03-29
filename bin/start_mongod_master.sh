d1828d0517b6a1d30f508e5c93db8049cc2866f8
Brannon Dorsey <brannon@brannondorsey.com>
2016-03-29 17:05:59 -0500

    Add bin/ for management scripts

---
+++ bin/start_mongod_master.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(basename $0)
+DIR_NAME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source "$DIR_NAME/env.sh"
+mongod \
+	--dbpath "$DIR_NAME/../data/db/" \
+	--port "$MONGOD_PORT" \
+	--master
---
+++ bin/tunnel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(basename $0)
+DIR_NAME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source "$DIR_NAME/env.sh"
+
+# NOTE: If connections established with this tunnel are failing
+# check the selected answer from this stack post:
+# http://serverfault.com/questions/478171/reverse-ssh-tunnel-connexion-refused
+ssh \
+	-R "0.0.0.0:$SSH_TUNNEL_REMOTE_PORT:127.0.0.1:$MONGOD_PORT" \
+	-i ~/.ssh/bdorse_15in_MacbookPro.pem \
+	"$SSH_TUNNEL_REMOTE_USER@$REMOTE_SERVER_HOST" \
+	-fNT
+
+echo $! # last PID
