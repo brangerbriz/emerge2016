aab3965d18323e1c0576b55612d410bccb0eba89
Brannon Dorsey <brannon@brannondorsey.com>
2016-04-01 13:40:37 -0500

    First try for gallery server start script

--- bin/start_gallery_server.sh
+++ bin/start_gallery_server.sh
@@ -11,3 +11,12 @@ source "$DIR_NAME/env.sh"
 # load the correct version of npm
 . "$NVM_DIR/nvm.sh" && nvm use "$NODE_VERSION_KINECT_DAEMON"
 
+# launch mongod
+("$DIR_NAME/start_mongod_slave.sh" 2>&1 & MONGOD_SLAVE_PID="$!")\
+	| prepend_timestamp \
+	| tee -a "$DIR_NAME/../log/mongod-slave.log" &
+
+# launch microsite node server
+node "$DIR_NAME/../microsite/server.js" 2>&1
+	| prepend_timestamp \
+	| tee -a "$DIR_NAME/../log/microsite-server.log" &
