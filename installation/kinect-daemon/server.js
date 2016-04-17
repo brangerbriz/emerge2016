10cfad0f95acd7087b7e96015a39f7b880dc3ec2
Brannon Dorsey <nickbriz@gmail.com>
2016-04-17 17:20:50 -0400

    Fix kinect-daemon listener leak bug

--- installation/kinect-daemon/server.js
+++ installation/kinect-daemon/server.js
@@ -1,3 +1,4 @@
+process.setMaxListeners(3);
 // App will crash after 11 connections :/
 // require('events').EventEmitter.defaultMaxListeners = Infinity; // HACK AND NOT WORKING
 var Kinect = require(__dirname + '/node-kinect/kinect');
@@ -12,15 +13,23 @@ var io = require('./../node_modules/socket.io')(server);
 kinect.start('depth');
 
 kinect.resume();
+kinect.setMaxListeners(0);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
-	// socket.setMaxListeners(0); // NOT WORKING
-	kinect.on('depth', function(buf) {
+
+	function kinectCallback(buf) {
 		socket.emit('kinect-depth', buf);
+	}
+
+	// socket.setMaxListeners(0); // NOT WORKING
+	kinect.on('depth', kinectCallback);
+
+	socket.on('disconnect', function(){
+		kinect.removeListener('depth', kinectCallback);
 	});
 });
 
