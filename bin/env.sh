6fb4a542b0e4bb93dd4d807fe845885176136300
Brannon Dorsey <nickbriz@gmail.com>
2016-04-22 17:26:16 -0400

    Change tunnel port

--- bin/env.sh
+++ bin/env.sh
@@ -3,7 +3,7 @@
 MONGOD_PORT=4004
 REMOTE_SERVER_HOST=labs.brangerbriz.com #labs 
 SSH_TUNNEL_REMOTE_USER=admin # username on labs server
-SSH_TUNNEL_REMOTE_PORT=7000 # port on labs server
+SSH_TUNNEL_REMOTE_PORT=7001 # port on labs server
 NODE_VERSION_KINECT_DAEMON=v0.10.25
 NODE_VERSION_MICROSITE=v0.12.3
 # SSH_TUNNEL_LOCAL_USER=ubuntu # username on installation machine behind NAT
